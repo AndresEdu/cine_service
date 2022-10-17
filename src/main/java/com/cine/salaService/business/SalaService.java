948ee17a88d59bbcea33634b40ac07162c984bb1
Luis Talavera <luisfertalavera15@gmail.com>
2022-10-17 12:05:08 -0500

    Escribe en archivo salas.csv

--- src/main/java/com/cine/salaService/business/SalaService.java
+++ src/main/java/com/cine/salaService/business/SalaService.java
@@ -1,9 +1,13 @@
 package com.cine.salaService.business;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.springframework.stereotype.Component;
 
 import com.cine.salaService.dto.Ack;
 import com.cine.salaService.dto.Sala;
+import com.opencsv.CSVWriter;
 
 
 @Component
@@ -11,10 +15,28 @@ public class SalaService {
 	public Ack createSala(Sala sala) {
 		Ack ack = new Ack();
 		
-		ack.setDescription("idSala" + sala.getIdSala() + "\n" +
-		"fila " + sala.getFila() + 
-		"columna " + sala.getColumna() +
-		"estado " + sala.getEstado());
+		try {
+			FileWriter writer = new FileWriter(".\\src\\main\\resources\\csv\\salas.csv", true);
+			CSVWriter csvWriter = new CSVWriter(writer);
+			
+			String[] linea = {
+					Integer.toString(sala.getIdSala()),
+					sala.getFila(),
+					Integer.toString(sala.getColumna()),
+					sala.getEstado()
+			};
+			
+			csvWriter.writeNext(linea);
+			
+			csvWriter.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		ack.setDescription("Se ha guardado la sala");
 		
 		ack.setCode(0);
 		
